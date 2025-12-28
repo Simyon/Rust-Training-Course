b4d7017651d1fe2fb24656434a8354fde6bae6da
Семён Красоткин <krasotkin.sa@phystech.edu>
2025-12-28 11:32:49 +0300

    Add implementation using the loop.

--- src/tasks/c3_ownership_and_memory.rs
+++ src/tasks/c3_ownership_and_memory.rs
@@ -36,9 +36,15 @@ pub fn string_ownership() {
 // show that it was not moved and still available.
 //
 // You can implement the function and use it right inside the `simple_borrowing` function.
+pub fn print_length(s: &String){
+    println!("The length of the string is {}", s.len());
+}
+
 #[allow(dead_code)]
 pub fn simple_borrowing() {
-    !unimplemented!()
+    let s : String = "count me ~nipa".to_string();
+    print_length(&s);
+    println!("The string is still here: {}", s);
 }
 
 // ----- 3 --------------------------------------
@@ -47,9 +53,17 @@ pub fn simple_borrowing() {
 // to check that the string was borrowed, not moved.
 //
 // You can implement the function and use it right inside the `hard_borrowing` function.
+pub fn append_and_return_length(string: &mut String, suffix: &str) -> usize {
+    string.push_str(suffix);
+    string.len()
+}
+
 #[allow(dead_code)]
 pub fn hard_borrowing() {
-    !unimplemented!()
+    let mut base_string = String::from("4el");
+    println!("The length of the string is {}", append_and_return_length(&mut base_string, " ty"));
+    println!("The length of the string is {}", append_and_return_length(&mut base_string, " w"));
+    println!("The length of the string is {}", append_and_return_length(&mut base_string, " mute."));
 }
 
 // SLICES
@@ -59,12 +73,105 @@ pub fn hard_borrowing() {
 // Write a function last_word(s: &str) -> &str that returns the last word from a string slice.
 // Assume words are separated by spaces.
 pub fn last_word(slice: &str) -> &str {
-    !unimplemented!()
+    //println!("slice: {}", slice);
+    if slice.len() == 0 {
+        return "";
+    }
+    let mut index = 0;
+    let mut space_index = 0;
+    let mut unspace_index : usize = 0;
+    'outter_loop: loop {
+        //println!("index: {}\tspace_index: {}\tunspace_index: {}", index, space_index, unspace_index);
+        if slice.as_bytes()[index] == b' ' {
+            space_index = index;
+        } else {
+            unspace_index = index;
+        }
+        index += 1; // Why Rust has no postfix increment operator;-(
+        if index == slice.len() {
+            //println!("Last index: {}\tspace_index: {}\tunspace_index: {}", index, space_index, unspace_index);
+            if space_index > unspace_index {
+                space_index = unspace_index;
+                'inner_loop: loop {
+                    if slice.as_bytes()[space_index] != b' ' {
+                        space_index -= 1;
+                    } else {
+                        break 'inner_loop;
+                    }
+                }
+            }
+            return &slice[space_index + 1..unspace_index + 1];
+        }
+    }
 }
 
 // ----- 5 --------------------------------------
 // Write a function longest_word(sentence: &str) -> &str that returns the longest word in a
 // sentence (string slice). If several words have the same maximum length, return the last one.
 pub fn longest_word(sentence: &str) -> &str {
-    !unimplemented!()
+    //println!("sentence: {}", sentence);
+    if sentence.len() == 0 {
+        return "";
+    }
+    let mut index = 0;
+    let mut space_index = 0;
+    let mut unspace_index : usize = 0;
+    let mut max_length = 0; 
+    let mut start_max_word_index = 0;
+    let mut end_max_word_index = 0;
+    'outter_loop: loop {
+        //println!("index: {}\tspace_index: {}\tunspace_index: {}", index, space_index, unspace_index);
+        if sentence.as_bytes()[index] == b' ' {
+            space_index = index;
+            if space_index > unspace_index && space_index - unspace_index == 1 {
+                space_index = unspace_index;
+                'inner_loop: loop {
+                    //println!("In inner_loop space_index: {}\tunspace_index: {}", space_index, unspace_index);
+                    if space_index == 0 {
+                        break 'inner_loop;
+                    }
+                    if sentence.as_bytes()[space_index] != b' ' {
+                        space_index -= 1;
+                    } else {
+                        break 'inner_loop;
+                    }                    
+                }
+                let length = unspace_index - space_index + 1;
+                if length >= max_length {
+                    max_length = length;
+                    if space_index == 0 {
+                        start_max_word_index = 0;
+                    } else {
+                        start_max_word_index = space_index + 1;
+                    }
+                    end_max_word_index = unspace_index + 1;
+                }
+            }
+            space_index = index;
+        } else {
+            unspace_index = index;
+        }
+        index += 1; 
+        if index == sentence.len() {
+            //println!("Last index: {}\tspace_index: {}\tunspace_index: {}", index, space_index, unspace_index);
+            if space_index > unspace_index {
+                space_index = unspace_index;
+                'inner_loop: loop {
+                    if sentence.as_bytes()[space_index] != b' ' {
+                        space_index -= 1;
+                    } else {
+                        break 'inner_loop;
+                    }
+                }
+            }
+            let length = unspace_index - space_index + 1;
+            //println!("length: {}\tmax_length: {}", length, max_length);
+            if length >= max_length {
+                max_length = length;
+                start_max_word_index = space_index + 1;
+                end_max_word_index = unspace_index + 1;
+            }
+            return &sentence[start_max_word_index..end_max_word_index];
+        }
+    }
 }
--- src/tests/c3.rs
+++ src/tests/c3.rs
@@ -1,9 +1,16 @@
-use crate::tasks::c3_ownership_and_memory::{longest_owned, string_ownership};
+use crate::tasks::c3_ownership_and_memory::{longest_owned, string_ownership, print_length, simple_borrowing, append_and_return_length, hard_borrowing, 
+    last_word, longest_word};
 
 #[test]
 fn test_longest_owned() {
-    assert_eq!("🙄🙏🏼😶‍🌫️".to_string(), longest_owned("🙄🙏🏼😶‍🌫️".to_string(), "😳🫣🌫🫥".to_string()));
-    assert_eq!("Arbeit macht frei!".to_string(), longest_owned("Arbeit macht frei!".to_string(), "Arbeit macht frei".to_string()));
+    assert_eq!(
+        "🙄🙏🏼😶‍🌫️".to_string(),
+        longest_owned("🙄🙏🏼😶‍🌫️".to_string(), "😳🫣🌫🫥".to_string())
+    );
+    assert_eq!(
+        "Arbeit macht frei!".to_string(),
+        longest_owned("Arbeit macht frei!".to_string(), "Arbeit macht frei".to_string())
+    );
     assert_eq!("ab0ba".to_string(), longest_owned("aboba".to_string(), "ab0ba".to_string()));
 }
 
@@ -12,7 +19,26 @@ fn test_string_ownership() {
     string_ownership();
 }
 
-/*
+#[test]
+fn test_print_length() {
+    print_length(&"1027384956".to_string());
+}
+
+#[test]
+fn test_simple_borrowing() {
+    simple_borrowing();
+}
+
+#[test]
+fn test_append_and_return_length() {
+    assert_eq!(16, append_and_return_length(&mut "I_Hate_Rust_".to_string(), "Joke"));
+}
+
+#[test]
+fn test_hard_borrowing() {
+    hard_borrowing();
+}
+
 #[test]
 fn test_last_word() {
     assert_eq!("kids", last_word("How do you do fellow kids"));
@@ -27,4 +53,3 @@ fn test_longest_word() {
     assert_eq!("Somebody", longest_word("Somebody once told me the world is gonna roll me"));
     assert_eq!("gonna", longest_word("Never gonna give you up Never gonna let you down"));
 }
-*/
